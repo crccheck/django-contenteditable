24c81a2703662b73be87db7c6bdb9cb30611b9fe
crccheck <c@crccheck.com>
2013-08-11 18:21:26 -0500

    update conf.urls import style for django 1.5

--- contenteditable/urls.py
+++ contenteditable/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 from . import views
+
 
 urlpatterns = patterns('',
     url(r'update/$', views.UpdateView.as_view(), name="dce_endpoint"),
--- example_project/newspaper/urls.py
+++ example_project/newspaper/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.views.generic import DetailView, ListView
 
 from newspaper.models import Article
+
 
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=Article), name='article_list'),
