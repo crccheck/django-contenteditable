992329c4aff0ae44ffccd8478922b0e70e7cda94
crccheck <c@crccheck.com>
2013-08-11 16:48:06 -0500

    update urls styles for django1.5, tests pass now

--- example_project/newspaper/models.py
+++ example_project/newspaper/models.py
@@ -1,14 +1,14 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField(default='This article has no text',
+    text = models.TextField(default=u'This article has no text',
                             blank=True, null=True)
 
     def __unicode__(self):
         return self.title
 
-    @models.permalink
     def get_absolute_url(self):
-        return ('article_detail', (), dict(pk=self.pk))
+        return reverse('article_detail', kwargs=dict(pk=self.pk))
